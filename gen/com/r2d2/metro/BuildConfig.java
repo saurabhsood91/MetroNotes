c14adbba32d815031bb127a65153da973ce9906e
Saurabh Sood <saurabhsood91@gmail.com>
2012-09-21 18:49:59 +0530

    initial commit

---
+++ gen/com/r2d2/metro/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.r2d2.metro;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/r2d2/metro/MainActivity.java
@@ -0,0 +1,26 @@
+package com.r2d2.metro;
+
+import org.apache.cordova.DroidGap;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.NavUtils;
+
+public class MainActivity extends DroidGap {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    
+}
