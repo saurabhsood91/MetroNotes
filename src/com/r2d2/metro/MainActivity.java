3e019dedf2ef01bf699b8dab8337e624cf39ee7c
Saurabh Sood <saurabhsood91@gmail.com>
2012-09-22 23:50:43 +0530

    added menu to create note

--- src/com/r2d2/metro/MainActivity.java
+++ src/com/r2d2/metro/MainActivity.java
@@ -5,6 +5,7 @@ import org.apache.cordova.DroidGap;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.support.v4.app.NavUtils;
 
@@ -13,14 +14,30 @@ public class MainActivity extends DroidGap {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        super.loadUrl("file:///android_asset/www/index.html");
+        super.loadUrl("file:///android_asset/www/main.html");
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
+    public boolean onCreateOptionsMenu(Menu menu) 
+    {
+    	MenuInflater inflator=getMenuInflater();
+    	
+        inflator.inflate(R.menu.activity_main, menu);
         return true;
     }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	// TODO Auto-generated method stub
+    	
+    	switch(item.getItemId()) {
+    		case R.id.item1 : 
+    			super.loadUrl("file:///android_asset/www/index.html");
+    			break;
+    	}
+    	return super.onOptionsItemSelected(item);
+    }
+    
 
     
 }
